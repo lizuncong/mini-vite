bce6d7daedb4ab64e7864387412ebd6321cadb6c
lizuncong <2900170896@qq.com>
2022-04-13 23:37:36 +0800

    feat: stash

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import virtualModule from './plugins/vite-plugin-example'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import virtualModule from './plugins/vite-plugin-example'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), virtualModule()]
+    resolve: {
+        alias: {
+            '@': resolve('./src'),
+            'comps': resolve('./src/components')
+        }
+    },
+    plugins: [vue(), virtualModule()]
 })
