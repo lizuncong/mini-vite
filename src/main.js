7b62e98e3a201723d2aabb202025bd660e87c1a8
lizuncong <2900170896@qq.com>
2022-04-16 17:18:49 +0800

    feat: ts

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import vm from 'virtual-module'
+import { mike } from './test.ts'
 console.log('vm====', vm)
+console.log('mike...', mike)
 // import './index.css'
 
 createApp(App).mount('#app')
---
+++ src/test.ts
@@ -0,0 +1,13 @@
+import pngUrl from './assets/logo.png'
+
+console.log('pngUrl...', pngUrl)
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+export const mike: Person = {
+    name: 'mike',
+    age: 23,
+}
